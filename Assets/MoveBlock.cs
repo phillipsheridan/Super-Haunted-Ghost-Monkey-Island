6841b364c2104b88d3bf267323fe78c29cd2b148
philsher93 <philsher93@gmail.com>
2016-04-02 18:48:32 -0400

    First commit

---
+++ Assets/MoveBlock.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace UnityStandardAssets.Utility
+{
+	public class MoveBlock : MonoBehaviour 
+{
+	public Transform DestinationSpot;
+	public Transform OriginSpot;
+	public float Speed;
+	public bool Switch = false;
+
+
+	void FixedUpdate () 
+	{
+		if (transform.position == DestinationSpot.position) {
+			Switch = true;
+
+	}
+		if (transform.position == OriginSpot.position) {
+			Switch = false;
+	}
+
+		if (Switch)
+		{	
+			transform.position = Vector3.MoveTowards (transform.position, OriginSpot.position, Speed);
+
+		}
+		else {
+			transform.position = Vector3.MoveTowards (transform.position, DestinationSpot.position, Speed);
+		}
+	}
+	}
+	
+
+	
+
+
+}
