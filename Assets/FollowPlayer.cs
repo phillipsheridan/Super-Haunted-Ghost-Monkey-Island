ac4f287169e905554becbccd9da34701817afacb
philsher93 <philsher93@gmail.com>
2016-04-08 23:59:17 -0400

    made a monkey follow script but it needs rotation.

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour {
+
+
+
+	public Transform Player;
+	public float speed;
+	public bool followingPlayer;
+	// Use this for initialization
+	void Start () {
+		this.followingPlayer = true;
+
+
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (followingPlayer) {
+			transform.position = Vector3.MoveTowards (transform.position, Player.position, speed);
+
+		}
+	
+	}
+}
