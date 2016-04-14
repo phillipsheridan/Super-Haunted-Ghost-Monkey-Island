ff5b03a73aa8fc42bf33b3ff4175cc587d02d50c
philsher93 <philsher93@gmail.com>
2016-04-13 22:21:41 -0400

    Added better music and new sounds, made three difficulties

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Audio;
 
 public class FollowPlayer : MonoBehaviour {
 
@@ -9,6 +10,7 @@ public class FollowPlayer : MonoBehaviour {
 	private Transform self;
 	public float moveSpeed = 3.0f;
 	public float rotationSpeed = 3.0f;
+	public AudioSource source;
 	// Use this for initialization
 	void Awake () {
 		self = transform;
@@ -20,11 +22,13 @@ public class FollowPlayer : MonoBehaviour {
 		target = GameObject.FindWithTag ("Player").transform;
 	}
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
 		if (target.gameObject.transform.position.y <= 1.3) { 
+			source.PlayDelayed(3);
 			self.rotation = Quaternion.Slerp (self.rotation, Quaternion.LookRotation (target.position - self.position), rotationSpeed * Time.deltaTime);
 			self.position += self.forward * moveSpeed * Time.deltaTime;
 
 		} 
+
 	}
 }
